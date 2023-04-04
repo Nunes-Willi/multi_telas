fd118ad86ca395b39cae9644e4775196f994db19
Nunes-Will <nuneswillroberto@gmail.com>
2023-04-04 16:19:00 -0300

    Create telas and add base app

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Provider as PaperProvider} from 'react-native-paper';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+
+          <Stack.telas name = "Home" component={Home}/>
+          <Stack.telas name = "Capcom" component={Capcom}/>
+          <Stack.telas name = "Eletronic Arts (EA)" component={EletronicArts}/>
+          <Stack.telas Name = "Nintendo" component={Nintendo}/>
+
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
